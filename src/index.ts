518bd92ab4a01763fc4d0de173fb0d27d785c8be
oukalatte <oukalatte@gmail.com>
2023-04-20 23:53:35 +0800

    pack with tsup

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import { Gtranz } from "./Gtranz";
 import useIsomorphicLayoutEffect from "./useIsomorphicLayoutEffect";
 import { useTimeline } from "./TransitionContext";
 
-export { useTimeline, useIsomorphicLayoutEffect };
-export default Gtranz;
+export { Gtranz, useTimeline, useIsomorphicLayoutEffect };
