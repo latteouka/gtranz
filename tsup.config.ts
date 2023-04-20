1eb85149fb9e3ced9bc01f0d1e52c538add8edcc
oukalatte <oukalatte@gmail.com>
2023-04-21 00:14:24 +0800

    pack with tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   treeshake: true,
-  sourcemap: "inline",
   minify: true,
   clean: true,
   dts: true,
   splitting: false,
   format: ["cjs", "esm"],
-  external: ["react"],
+  external: ["react", "gsap"],
 });
